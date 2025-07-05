a283a376f939f87f2a398788048b599621592b82
Naved <navaidmals@gmail.com>
2025-07-05 07:46:01 +0530

    qr

--- day-15/modelFormProject/testapp/admin.py
+++ day-15/modelFormProject/testapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from testapp.models import Student
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name','rollno','marks']
+
+admin.site.register(Student,StudentAdmin)
+
